d267c665a4273388359f854571621f6b007e27cf
Kundyz <kundyz94@gmail.com>
2023-02-23 16:30:47 +0500

    Создала первый массив, который вводит пользователь

--- Task/Program.cs
+++ Task/Program.cs
@@ -6,3 +6,13 @@ Console.Write(" -> ");
 string[] secondArray = SecondArray(1, 3, firstArray);
 PrintArray(secondArray);
 
+string[] FirstArray(int lenghtArray)
+{
+    string[] array = new string[lenghtArray];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите элемент массива");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
