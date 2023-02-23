4383afca09640682eaf2ed33ca01cb66dadc21e7
Kundyz <kundyz94@gmail.com>
2023-02-23 16:30:05 +0500

    Написала тело алгоритма

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,8 @@
-﻿
+﻿Console.WriteLine("Задайте длину массива");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] firstArray = FirstArray(size);
+PrintArray(firstArray);
+Console.Write(" -> ");
+string[] secondArray = SecondArray(1, 3, firstArray);
+PrintArray(secondArray);
 
