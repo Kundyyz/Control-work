5e74dd4e05f030cbf13a9b5f0471161598372be4
Kundyz <kundyz94@gmail.com>
2023-02-23 16:31:38 +0500

    Создала метод войд для выведения массива в консоль

--- Task/Program.cs
+++ Task/Program.cs
@@ -16,3 +16,15 @@ string[] FirstArray(int lenghtArray)
     }
     return array;
 }
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("] ");
+}
+
